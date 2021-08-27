e0d3b17676b11a6819b73b8dc253886f02c66a29
Mat Moore <MatMoore@users.noreply.github.com>
2021-08-27 14:49:23 +0100

    Add some tests to characterise the behaviour of sell_in

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -8,6 +8,24 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "reduces the sell in date of the item if it is not Sulfuras" do
+      items = [Item.new("foo", 1, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+    end
+
+    it "reduces the sell in date to a negative number if it is zero" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq -1
+    end
+
+    it "does not reduce the sell in date for Sulturas" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+    end
   end
 
 end
