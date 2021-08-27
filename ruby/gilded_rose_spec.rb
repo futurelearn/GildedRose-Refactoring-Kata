9f501e52c65a3b02323b47530dea8b14f413b48b
Mat Moore <MatMoore@users.noreply.github.com>
2021-08-27 14:54:44 +0100

    Add some tests for quality changes
    This doesn't cover all the behaviour but captures the basic requirement
    of quality generally decreasing over time.

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -26,6 +26,24 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 0
     end
+
+    describe "goods selling in 12 days or more" do
+      describe "aged brie" do
+        it "increases in quality with age" do
+          items = [Item.new("Aged Brie", 12, 1)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 2
+        end
+      end
+
+      describe "a degradable good" do
+        it "decreases in quality with age" do
+          items = [Item.new("foo", 12, 2)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 1
+        end
+      end
+    end
   end
 
 end
