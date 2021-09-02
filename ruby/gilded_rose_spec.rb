eb7ec38b2d96d0b65963b511a9e8136f1f7f81ec
jakeprime <jake.prime@futurelearn.com>
2021-09-02 11:30:15 +0100

    Add simple test for Aged Brie

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -42,6 +42,15 @@ describe GildedRose do
         expect(items[0].sell_in).to eq 9
       end
     end
+
+    describe "Aged Brie" do
+      it "increases in quality" do
+        items = [Item.new("Aged Brie", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 11
+      end
+
+    end
   end
 
 end
