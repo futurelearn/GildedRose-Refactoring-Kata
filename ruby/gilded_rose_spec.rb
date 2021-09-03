cb35469ea25d959ddc22b6053ccf403273bc31c7
Simon Coffey <simon.coffey@futurelearn.com>
2021-09-03 09:29:00 +0100

    Restructure tests
    Extracting test setup will help clarify the individual cases as we add
    tests.

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,18 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  let(:name) { "foo" }
+  let(:sell_in) { 5 }
+  let(:quality) { 10 }
+  let(:item) { Item.new(name, sell_in, quality) }
+
+  subject { described_class.new([item]) }
+
+  before { subject.update_quality }
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(item.name).to eq "foo"
     end
   end
-
 end
