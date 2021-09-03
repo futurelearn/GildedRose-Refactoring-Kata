f9e5053874075c7d2039ab7ccaa4ae43a1565710
Simon Coffey <simon.coffey@futurelearn.com>
2021-09-03 09:29:22 +0100

    Extract per-item strategy objects
    In the previous commit I extracted a single strategy object that just
    used the same case statement as the GildedRose object had; this now
    splits it into individual strategy objects for specific items.
    
    If the goblin let me modify the item class I'd almost certainly have
    done that instead, but the goblin is as the goblin does.
    
    In the single case of this file, this extraction doesn't really
    accomplish much; we've really just made the case statement a bit harder
    to read, if I'm honest.
    
    This sort of refactoring really comes into its own though if the objects
    instantiated by the case statement are passed on to other bits of a more
    complex system; it can be useful to have these named concepts in place
    because it makes it easier to implement new, polymorphically varying
    behaviour in future.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,34 +6,59 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      DailyUpdateStrategy.new(item).do_daily_update
+      DailyUpdateStrategy.for(item).do_daily_update
     end
   end
 end
 
 class DailyUpdateStrategy
-  def initialize(item)
-    @item = item
+
+  class Sulfuras < DailyUpdateStrategy
+    def do_daily_update
+    end
   end
 
-  def do_daily_update
-    case item.name
-    when "Sulfuras, Hand of Ragnaros"
-    when "Aged Brie"
+  class Brie < DailyUpdateStrategy
+    def do_daily_update
       item.increment_quality
       item.increment_quality if item.sell_in <= 0
       item.decrement_sell_in
-    when "Backstage passes to a TAFKAL80ETC concert"
+    end
+  end
+
+  class BackstagePass < DailyUpdateStrategy
+    def do_daily_update
       item.increment_quality
       item.increment_quality if item.sell_in < 11
       item.increment_quality if item.sell_in < 6
       item.quality = 0 if item.sell_in <= 0
       item.decrement_sell_in
-    else
+    end
+  end
+
+  class UnknownItem < DailyUpdateStrategy
+    def do_daily_update
       item.decrement_quality
       item.decrement_quality if item.sell_in <= 0
       item.decrement_sell_in
     end
+  end
+
+  def self.for(item)
+    case item.name
+    when "Sulfuras, Hand of Ragnaros" then Sulfuras
+    when "Aged Brie" then Brie
+    when "Backstage passes to a TAFKAL80ETC concert" then BackstagePass
+    else UnknownItem
+    end.new(item)
+  end
+
+  def initialize(item)
+    @item = item
+  end
+
+  def do_daily_update
+    fail NotImplementedError
   end
 
   private
