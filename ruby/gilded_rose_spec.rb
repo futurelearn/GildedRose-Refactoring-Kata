9a9c27624b4c9ee8dcac93aa1ea4a9369b1a621a
Simon Coffey <simon.coffey@futurelearn.com>
2021-09-03 09:29:01 +0100

    Add non-special item base case
    The standard case: an unknown item, before the sell-in has passed, with
    positive quality.

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -14,5 +14,17 @@ describe GildedRose do
     it "does not change the name" do
       expect(item.name).to eq "foo"
     end
+
+    context "for a non-special item" do
+      let(:name) { "not-a-special-case" }
+
+      it "decrements the sell-in date" do
+        expect(item.sell_in).to eq 4
+      end
+
+      it "decrements the quality" do
+        expect(item.quality).to eq 9
+      end
+    end
   end
 end
